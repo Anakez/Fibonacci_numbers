64396eefd0e81a1661ec6d08b0a879d0f30c4e4c
Anakez <72194546+Anakez@users.noreply.github.com>
2021-03-05 13:00:39 +0800

    Update Fib.cpp

--- Fib.cpp
+++ Fib.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int* Fib(int size_arr)
+int size_arr = 100; 
+int* Fib(void)
 {
 	int* arr = new int[size_arr];
 	arr[0] = arr[1] = 1;
-	for (int i = 2; i < size_arr; i++)
+	for (int i = 2; i < size_arr; i++) {
+		if (INT_MAX - arr[i - 2] < arr[i - 1])
+		{
+			size_arr = i;
+			break;
+		}
 		arr[i] = arr[i - 1] + arr[i - 2];
+	}
 	return arr;
 }
 int main()
 {
-	int i, size_arr;
-	cout << "Enter how many fibonnach numbers you need = ";
-	cin >> size_arr;
+	int i;
+	int* fibArr = Fib();
 	cout << "Fibonacci numbers: " << endl;
 	for (i = 0; i < size_arr; i++)
-	int* fibArr = Fib(size_arr);
-	{ 
 		cout << fibArr[i] << endl;
-		if (INT_MAX - fibArr[i - 1] < fibArr[i])
-		{
-			cout << "The number is too large"<<endl;
-			break;
-		}
-	}
+	if (size_arr < 100)
+		cout << "The number is too large" << endl;
 	cout << "The last fibonacci number: " << i << endl;
 	delete[] fibArr;
 	return 0;
